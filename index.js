a8c38eb52b4e262ba442505809bad8d16472ed1a
juarezjunior256 <juarezjunior.live@gmail.com>
2021-08-01 01:22:36 -0300

    Formulário pronto para enviar para o banco

--- index.js
+++ index.js
@@ -1,9 +1,12 @@
 const express = require("express");
 const app = express();
+const bodyParser = require("body-parser");
 
 // habilita o express a renderizar o html usando ejs
 app.set("view engine", "ejs");
 app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
 
 app.get("/", (req, res) => {
   res.render("index");
@@ -11,6 +14,13 @@ app.get("/", (req, res) => {
 
 app.get("/perguntar", (req, res) => {
   res.render("perguntar");
+});
+
+app.post("/salvarpergunta", (req, res) => {
+  var titulo = req.body.titulo;
+  var descricao = req.body.descricao;
+
+  res.send(`titulo: ${titulo}, descrição: ${descricao}`);
 });
 
 app.listen(8080, () => {
